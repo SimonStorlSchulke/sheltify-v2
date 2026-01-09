7444890f5d3ded849f4dad08932ab0bbf70cba20
SimonStorlSchulke <simonstoschu@googlemail.com>
2026-01-10 00:03:39 +0100

    render columns preview

--- sheltify-admin/src/app/section-renderer/section-renderer.component.ts
+++ sheltify-admin/src/app/section-renderer/section-renderer.component.ts
@@ -1,3 +1,4 @@
+import { NgTemplateOutlet } from '@angular/common';
 import { Component, CUSTOM_ELEMENTS_SCHEMA, inject, input } from '@angular/core';
 import { DomSanitizer } from '@angular/platform-browser';
 import { Section } from 'sheltify-lib/article-types';
@@ -5,13 +6,13 @@ import { CmsRequestService } from 'src/app/services/cms-request.service';
 
 @Component({
   selector: 'app-section-renderer',
-  imports: [],
+  imports: [NgTemplateOutlet],
   templateUrl: './section-renderer.component.html',
   styleUrl: './section-renderer.component.scss',
   schemas: [CUSTOM_ELEMENTS_SCHEMA],
 })
 export class SectionRendererComponent {
-  public section = input<Section>();
+  public section = input.required<Section>();
   public uploadsUrl = CmsRequestService.publicApiUrl + 'uploads/';
 
   public domSanitizer= inject(DomSanitizer);
