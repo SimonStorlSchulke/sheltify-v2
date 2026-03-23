32fcf2d36ed0fd887b619ccc9a33289d56da7bb9
SimonStorlSchulke <simonstoschu@googlemail.com>
2026-03-23 07:24:51 +0100

    init mft theme

--- clients/shared/cms/cms-image.ts
+++ clients/shared/cms/cms-image.ts
@@ -2,8 +2,15 @@ import { cms } from '@shared/cms/sheltify-access.ts';
 import type { CmsImage, CmsImagesSize } from 'sheltify-lib/cms-types.ts';
 
 export function getImageSrc(image: CmsImage, requestedSize: CmsImagesSize): string {
+  if(image.OriginalFileName.endsWith('.svg')) {
+    return `${cms.uploadsUrl}${image.ID}.svg`;
+  }
   const availableSize = getLargestAvailableImageSize(requestedSize, image);
   return `${cms.uploadsUrl}${image.ID}_${availableSize}.webp`;
+}
+
+export function getSvgSrc(image: CmsImage): string {
+  return `${cms.uploadsUrl}${image.ID}`;
 }
 
 export function getLargestAvailableImageSize(requestedSize: CmsImagesSize, image: CmsImage): CmsImagesSize {
